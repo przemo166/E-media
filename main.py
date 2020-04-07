bdf8ee6347b7afa20921a9a4b515889e0a1d4108
PrzemekWidz <przemek@przemek-Latitude-E7450>
2020-04-07 20:53:15 +0200

    Showing images is working

--- main.py
+++ main.py
@@ -1,3 +1,15 @@
 #!/usr/bin/python3
 
-print("Hello World!")
+# Importing modules to show png image
+import matplotlib.image as mpimg
+import matplotlib.pyplot as plt
+from PIL import Image
+
+####################
+#   Main file      #
+####################
+
+imageName = input("File name : ")
+
+img = Image.open('example_files/{}'.format(imageName))
+img.show()
