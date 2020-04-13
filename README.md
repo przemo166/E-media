00a3b22f4342c97a55171355032c44cbb0c220d2
PrzemekWidz <przemek@przemek-Latitude-E7450>
2020-04-13 21:45:12 +0200

    Added PLTE chunk reader in girl.png file

--- README.md
+++ README.md
@@ -4,15 +4,15 @@ Wybrany format plików : PNG
 
 # 1.Show image function 
 
-![Zdjęcie projektu ](readme_images/showImage.png "fig:") 
+![Zdjęcie projektu ](github_images/showImage.png "fig:") 
 [fig:my~l~abel]
 
 # 2.Fourier transform function
 
-![Zdjęcie projektu ](readme_images/fourier.png "fig:") 
+![Zdjęcie projektu ](github_images/fourier.png "fig:") 
 [fig:my~l~abel]
 
 # 3.IHDR analizing function
 
-![Zdjęcie projektu ](readme_images/IHDR.png "fig:") 
+![Zdjęcie projektu ](github_images/IHDR.png "fig:") 
 [fig:my~l~abel]
--- functions/analizePLTE.py
+++ functions/analizePLTE.py
@@ -13,11 +13,44 @@ def analizePLTE (hexArray):
 
     if position!=-1:
         print("\nPLTE found")
-        position += 8
 
         print("\n######################")
         print("# Reading PLTE chunk #")
         print("######################")
+
+        chunkLenght = hexArray[(position-8):position]
+        chunkLenghtDecimal = int(chunkLenght,16)
+        position+=8
+
+        i = 0
+
+        print("\n")
+
+        f = open("data/data_PLTE_chunk.txt", "w")
+
+        while i < chunkLenghtDecimal/3:
+
+            tmpRed = hexArray[position:position+2]
+            tmpRedDecimal = int(tmpRed,16)
+            position+=2
+
+            tmpGreen = hexArray[position:position+2]
+            tmpGreenDecimal = int(tmpGreen,16)
+            position+=2
+
+            tmpBlue = hexArray[position:position+2]
+            tmpBlueDecimal = int(tmpBlue,16)
+            position+=2
+
+            var = "Palete entry " + str(i) + " " + "| Red : " + str(tmpRedDecimal) + " Green : " + str(tmpGreenDecimal) + " Blue : " + str(tmpBlueDecimal) + " |"
+            f.write( var + "\n"  )
+            #print(var)
+
+            i+=1
+
+        f.close()
+
+        print("Entries saved in .txt file in /data directory\n")
 
     else:
         print("\nPLTE not found")
